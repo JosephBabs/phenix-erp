a0491810ebdcc2b3be186d6b333ee309d37a93e8
JosephBabs <bajos3d@gmail.com>
2025-01-10 14:22:28 +0100

    css update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
     <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/2.6.0/uicons-regular-straight/css/uicons-regular-straight.css'>
     <link rel="stylesheet" href="/bootstrap/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="/bootstrap/dist/css/bootstrap.css">
-    <!-- <link rel="stylesheet" href="/bootstrap/dist/mobile.css"> -->
+    <link rel="stylesheet" href="/bootstrap/dist/main.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
@@ -32,7 +32,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 
     <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
-    <!-- <link rel="stylesheet" href="../assets/css/atlantis.css"> -->
+    <link rel="stylesheet" href="../assets/css/atlantis.css">
     <link rel="stylesheet" href="../assets/css/demo.css">
 
 </head>
