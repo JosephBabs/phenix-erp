0f2d5d619eb9842474f8a4336a9e2220b074cf0c
JosephBabs <bajos3d@gmail.com>
2025-01-10 14:23:32 +0100

    remove atlantis css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,7 +32,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 
     <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../assets/css/atlantis.css">
+    <!-- <link rel="stylesheet" href="../assets/css/atlantis.css"> -->
     <link rel="stylesheet" href="../assets/css/demo.css">
 
 </head>
